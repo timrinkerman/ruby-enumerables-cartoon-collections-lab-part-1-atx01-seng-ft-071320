cb0006201693e5e6fdb32e1ee2888ce7f8a61d2b
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-15 01:35:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 array.each_with_index do |name, index|
-  index = index.int + 1 
+  index = index + 1 
   puts "#{index} #{name}"
 end
 end
